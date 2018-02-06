77e20c4b877a46d29b940422ac1e45b28b7f3ea6
Carlos Rodrigues <carlos.rodrigues@iplicit.com>
2018-02-06 12:09:09 +0000

    using nuxtjs

--- README.md
+++ README.md
@@ -1,44 +1,23 @@
-# GinCloud 
-[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-url]][daviddm-image] [![Coverage Status][coveralls-image]][coveralls-url]
+# gin-manga
 
-The best module ever.
+> A Vue.js project
 
+## Build Setup
 
-## Install
+``` bash
+# install dependencies
+npm install
 
-```bash
-$ npm install --save gincloud
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
 ```
 
-
-## Usage
-
-```javascript
-var gincloud = require('gincloud');
-gincloud(); // "awesome"
-```
-
-## API
-
-_(Coming soon)_
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 
-## Contributing
-
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [gulp](http://gulpjs.com/).
-
-
-## License
-
-Copyright (c) 2014 Carlos Rodrigues. Licensed under the MIT license.
-
-
-
-[npm-url]: https://npmjs.org/package/gincloud
-[npm-image]: https://badge.fury.io/js/gincloud.svg
-[travis-url]: https://travis-ci.org/pikax/gincloud
-[travis-image]: https://travis-ci.org/pikax/gincloud.svg?branch=master
-[daviddm-url]: https://david-dm.org/pikax/gincloud.svg?theme=shields.io
-[daviddm-image]: https://david-dm.org/pikax/gincloud
-[coveralls-url]: https://coveralls.io/r/pikax/gincloud
-[coveralls-image]: https://coveralls.io/repos/pikax/gincloud/badge.png
---
+++ nuxt.config.js
@@ -0,0 +1,122 @@
+const pkg = require('./package')
+
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: pkg.name + ' %s',
+    meta: [
+      {charset: 'utf-8'},
+      {
+        property: 'og:image',
+        content: 'https://user-images.githubusercontent.com/904724/26879447-689b56a8-4b91-11e7-968f-5eea1d6c71b4.png'
+      },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:site', content: '@nuxt_js' },
+    ],
+
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: {color: '#3B8070'},
+
+  loadingIndicator: {
+    name: 'rectangle-bounce',
+    color: 'white',
+    background: '#188269'
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'vuetify/src/stylus/main.styl'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vuetify',
+    { src: '~/plugins/localStorage.js', ssr: false },
+    { src: '~/plugins/networkStatus.ts', ssr: false },
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '~modules/typescript.js',
+    '@nuxtjs/pwa',
+    '@nuxtjs/component-cache',
+  ],
+
+
+  router: {
+    middleware: ['https']
+  },
+
+  /*
+  ** PWA manifest
+  */
+
+  manifest: {
+    name: 'gincloud',
+    description: 'manga website',
+ /*   short_name: 'PikaxApp',
+    start_url: '/',
+    display: "standalone",
+    lang: 'en',
+*/
+    theme_color: '#3B8070'
+  },
+
+	//
+  // workbox:{
+  //   importScripts: [
+  //     'sw.js'
+  //   ],
+  // },
+
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
+    }
+  },
+
+  render: {
+    static: {
+      maxAge: '1y',
+      setHeaders (res, path) {
+        if (path.includes('sw.js')) {
+          res.setHeader('Cache-Control', 'public, max-age=0')
+        }
+      }
+    }
+  }
+}
